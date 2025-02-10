67426555fcfe6bc8cf647d2c02d2e7f390d56861
Nettan <jeanette.norberg82@gmail.com>
2025-02-10 14:57:14 +0100

    changed main activity to home activity, for that i needed to change in manifest. added buttons and a image, and fragmentcontainerview

--- app/src/main/java/com/example/buddybloom/HomeActivity.kt
+++ app/src/main/java/com/example/buddybloom/HomeActivity.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
