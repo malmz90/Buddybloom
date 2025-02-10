edd824cd4fc6bf4a4a6b3547010e5f2b12e7f82c
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2025-02-10 15:09:28 +0100

    Added Video View to start activity showing the start logo.

--- app/src/main/java/com/example/buddybloom/StartActivity.kt
+++ app/src/main/java/com/example/buddybloom/StartActivity.kt
@@ -1,12 +1,17 @@
 package com.example.buddybloom
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+//TODO Set as launcher activity when done
 class StartActivity : AppCompatActivity() {
+    private lateinit var videoView: VideoView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,15 @@ class StartActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        videoView = findViewById(R.id.vv_splash)
+        val videoUri = Uri.parse("android.resource://$packageName/${R.raw.splash}")
+        videoView.setVideoURI(videoUri)
+        videoView.setOnCompletionListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        videoView.start()
     }
 }
