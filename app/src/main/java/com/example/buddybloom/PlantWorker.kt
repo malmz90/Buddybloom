cdfae0c84bec8ef8d17ba3a6fbb6f178026723de
Nettan <jeanette.norberg82@gmail.com>
2025-02-12 13:32:43 +0100

    added functions for waterLevel and PlantWorkerClass

---
+++ app/src/main/java/com/example/buddybloom/PlantWorker.kt
@@ -0,0 +1,18 @@
+package com.example.buddybloom
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class PlantWorker(appContext: Context, workerParams: WorkerParameters):
+    Worker(appContext, workerParams) {
+    override fun doWork(): Result {
+        val myPlant = Plant("",2,100)
+        myPlant.decreaseWaterLevel(10)
+        myPlant.isThirsty()
+    // Uppdatera UI eller spara data om det behövs
+
+    // Returnera Result.success() när jobbet är klart
+    return Result.success()
+}
+}
