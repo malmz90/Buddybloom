488ae2ebfa3d5748a45b802e889dd85f5546eb22
FridaEriksson95 <fridaeriiksson@gmail.com>
2025-02-12 13:31:37 +0100

    added design for dialog boxes

--- app/src/main/java/com/example/buddybloom/StartPagePlantFragment.kt
+++ app/src/main/java/com/example/buddybloom/StartPagePlantFragment.kt
@@ -5,55 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.buddybloom.databinding.FragmentStartPagePlantBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StartPagePlantFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StartPagePlantFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var binding : FragmentStartPagePlantBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start_page_plant, container, false)
+        binding = FragmentStartPagePlantBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StartPagePlantFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StartPagePlantFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.switchBlinds?.setOnClickListener {
+            Toast.makeText(requireContext(), "You've successfully protected your plant!", Toast.LENGTH_SHORT).show()
+        }
+
+        binding?.imgBtnWaterspray?.setOnClickListener {
+            Toast.makeText(requireContext(), "You've successfully sprayed water on your plant!", Toast.LENGTH_SHORT).show()
+        }
+        binding?.imgBtnBugspray?.setOnClickListener {
+            Toast.makeText(requireContext(), "You've successfully saved your plant from bugs!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
