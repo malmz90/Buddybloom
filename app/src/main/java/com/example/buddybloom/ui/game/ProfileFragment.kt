678e3e0ef124fedd7a052d719f3ba554dfdae1f3
Nettan <jeanette.norberg82@gmail.com>
2025-02-18 08:34:13 +0100

    Added toast on/Off to notificationbutton in profileFragment

--- app/src/main/java/com/example/buddybloom/ui/game/ProfileFragment.kt
+++ app/src/main/java/com/example/buddybloom/ui/game/ProfileFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.buddybloom.databinding.FragmentProfileBinding
 import com.example.buddybloom.ui.authentication.AccountViewModel
@@ -31,6 +32,15 @@ class ProfileFragment : Fragment() {
         binding.deleteAccountText.setOnClickListener {
             val deleteAccountDialog = DeleteAccountDialogFragment()
             deleteAccountDialog.show(parentFragmentManager, "DeleteAccountDialogFragment")
+        }
+        binding.dailyNotificationsSwitch.setOnCheckedChangeListener{ _,isChecked ->
+            if(isChecked){
+                Toast.makeText(requireContext(),"Notifications ON!!", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(requireContext(),"Notifications OFF!",Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
