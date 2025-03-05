dd257f107e873ead6b240475a654219fd44ed715
FridaEriksson95 <fridaeriiksson@gmail.com>
2025-03-05 10:56:30 +0100

    Moved logic for pictures stages to a new class PlantImageManager and fixed plant dead sound.

--- app/src/main/java/com/example/buddybloom/ui/game/StartPagePlantFragment.kt
+++ app/src/main/java/com/example/buddybloom/ui/game/StartPagePlantFragment.kt
@@ -62,6 +62,8 @@ class StartPagePlantFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val plantImageManager = PlantImageManager(requireContext(), binding)
+
         pvm.errorMessage.observe(viewLifecycleOwner) { message ->
             message?.let {
                 Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
@@ -78,13 +80,14 @@ class StartPagePlantFragment : Fragment() {
 
         pvm.plantJustDied.observe(viewLifecycleOwner) { justDied ->
             if(justDied) {
-                checkPlantDeath()
+                pvm.localSessionPlant.value?.let { plant ->
+                    plantImageManager.checkPlantDeath(plant, binding)
+                }
                 pvm.resetPlantDeathState()
             }
         }
 
         pvm.localSessionPlant.observe(viewLifecycleOwner) { plant ->
-
             //Checks if user is logging in
             testPlant = plant
 
@@ -93,7 +96,7 @@ class StartPagePlantFragment : Fragment() {
 
             if (plant == null) return@observe
             if (avm.isLoggingIn.value != true) {
-                binding.imgFlower.setImageResource(getPlantImageId(plant))
+                binding.imgFlower.setImageResource(plantImageManager.getPlantImageId(plant))
                 binding.tvDaystreak.text = String.format(getDaysOld(plant).toString())
                 //Progress indicator
                 "${plant.waterLevel}%".also { binding.tvWaterLevel.text = it }
@@ -110,6 +113,7 @@ class StartPagePlantFragment : Fragment() {
                                 .start()
                         }
                         .start()
+                    plantImageManager.showInfectedBugGif(plant)
                 } else {
                     binding.imgBtnBugspray.clearAnimation()
                     binding.imgBtnBugspray.background =
@@ -141,7 +145,8 @@ class StartPagePlantFragment : Fragment() {
         blindsSoundEnd = soundPool.load(requireContext(), R.raw.blinds_sound_end, 1)
         bugSpraySound = soundPool.load(requireContext(), R.raw.bugspray_sound, 1)
         errorSound = soundPool.load(requireContext(),R.raw.error_sound,1)
-        showInfectedBugGif()// if it`s infected gif shows even if restart app
+        pvm.localSessionPlant.value?.let { plant ->
+        plantImageManager.showInfectedBugGif(plant)}// if it`s infected gif shows even if restart app
 
         binding.apply {
 
@@ -173,8 +178,11 @@ class StartPagePlantFragment : Fragment() {
                         binding.btnWater.setTextColor(Color.parseColor("#246246"))
                         binding.btnWater.isEnabled = true
                         pvm.waterPlant()
-                        checkPlantDeath()
-                        showInfectedBugGif()}
+                        pvm.localSessionPlant.value?.let { plant ->
+                            plantImageManager.checkPlantDeath(plant, binding)
+                            plantImageManager.showInfectedBugGif(plant)
+                        }
+                    }
                         , 3000)
                 }
             }
@@ -195,7 +203,8 @@ class StartPagePlantFragment : Fragment() {
                     Handler(Looper.getMainLooper()).postDelayed({
                         binding.ivAnimationWateringCan.visibility = View.INVISIBLE
                         pvm.waterSpray()
-                        checkPlantDeath()
+                        pvm.localSessionPlant.value?.let { plant ->
+                            plantImageManager.checkPlantDeath(plant, binding)}
                     }, 3000)
 
 
@@ -221,7 +230,8 @@ class StartPagePlantFragment : Fragment() {
                     Handler(Looper.getMainLooper()).postDelayed({
                         binding.ivAnimationWateringCan.visibility = View.INVISIBLE
                         pvm.fertilizePlant()
-                        checkPlantDeath()
+                        pvm.localSessionPlant.value?.let { plant ->
+                        plantImageManager.checkPlantDeath(plant, binding)}
                     }, 3000)
                 }
             }
@@ -320,183 +330,12 @@ class StartPagePlantFragment : Fragment() {
         }
     }
 
-    /**
-     *  function shows infected gif
-      */
-    private fun showInfectedBugGif() {
-        pvm.localSessionPlant.value?.let { currentPlant ->
-            if (currentPlant.infected) {
-                val bugDrawable: Drawable? = ContextCompat.getDrawable(
-                    requireContext(),
-                    R.drawable.gif_infected_bugs
-                )
-                if (bugDrawable is AnimatedImageDrawable) {
-                    binding.ivInfectedBug.visibility = View.VISIBLE
-                    binding.ivInfectedBug.setImageDrawable(bugDrawable)
-                    bugDrawable.start()
-                }
-            }
-        }
-    }
-//*******************
+    //*******************
     private fun updateBlindsVisibility(myImageView: View) {
         if (testPlant?.protectedFromSun == true) {
             myImageView.visibility = View.VISIBLE
         } else {
             myImageView.visibility = View.INVISIBLE
-        }
-    }
-//*******************
-
-    private fun getPlantStage(plant:Plant):Int {
-        val daysOld = ((System.currentTimeMillis() - (plant.createdAt.seconds * 1000)) / (1000 * 60 * 60 * 24))
-
-        return when {
-            daysOld >= 5 -> 6
-            daysOld >= 4 -> 5
-            daysOld >= 3 -> 4
-            daysOld >= 2 -> 3
-            daysOld >= 1 -> 2
-            else -> 1
-        }
-    }
-
-    /**
-     * Returns the correct image based on how old the plant is and in which stage it dies.
-     */
-    private fun getPlantImageId(plant: Plant?, deathStage: Int? = null): Int {
-        if (plant == null) return R.drawable.icon_obs
-
-        // Check if plant is dried (water level below 30)
-        if (plant.waterLevel < 30) {
-            val stage = deathStage ?: getPlantStage(plant)
-            return when (plant.name.lowercase()) {
-                "elephant" ->  when(stage) {
-                    1 -> R.drawable.flower_elephant_dead1_2
-                    2 -> R.drawable.flower_elephant_dead1_2
-                    3 -> R.drawable.flower_elephant_dead3
-                    4 -> R.drawable.flower_elephant_dead4
-                    5 -> R.drawable.flower_elephant_dead5
-                    6 -> R.drawable.flower_elephant_dead6
-                    else -> R.drawable.flower_elephant_dead1_2
-                }
-                "hibiscus" -> when(stage) {
-                    1 -> R.drawable.flower_hibiscus_dead1
-                    2 -> R.drawable.flower_hibiscus_dead2_3
-                    3 -> R.drawable.flower_hibiscus_dead2_3
-                    4 -> R.drawable.flower_hibiscus_dead4
-                    5 -> R.drawable.flower_hibiscus_dead5
-                    6 -> R.drawable.flower_hibiscus_dead5
-                    else -> R.drawable.flower_hibiscus_dead1
-                }
-                "zebra" -> when(stage) {
-                    1 -> R.drawable.flower_zebra_dead1
-                    2 -> R.drawable.flower_zebra_dead2
-                    3 -> R.drawable.flower_zebra_dead3
-                    4 -> R.drawable.flower_zebra_dead4
-                    5 -> R.drawable.flower_zebra_dead5
-                    6 -> R.drawable.flower_zebra_dead6
-                    else -> R.drawable.flower_zebra_dead1
-                }
-                "ficus" -> when(stage) {
-                    1 -> R.drawable.flower_ficus_dead1_2
-                    2 -> R.drawable.flower_ficus_dead1_2
-                    3 -> R.drawable.flower_ficus_dead3
-                    4 -> R.drawable.flower_ficus_dead4
-                    5 -> R.drawable.flower_ficus_dead5
-                    6 -> R.drawable.flower_ficus_dead6
-                    else -> R.drawable.flower_ficus_dead1_2
-                }
-                "coleus" -> when(stage) {
-                    1 -> R.drawable.flower_coleus_dead1
-                    2 -> R.drawable.flower_coleus_dead2
-                    3 -> R.drawable.flower_coleus_dead3
-                    4 -> R.drawable.flower_coleus_dead4
-                    5 -> R.drawable.flower_coleus_dead5
-                    6 -> R.drawable.flower_coleus_dead5
-                    else -> R.drawable.flower_coleus_dead1
-                }
-                else -> R.drawable.flower_elephant_dead1_2
-            }
-        }
-
-        val daysOld =
-            ((System.currentTimeMillis() - (plant.createdAt.seconds * 1000)) / (1000 * 60 * 60 * 24))
-
-        val stage = when {
-                daysOld >= 5 -> 6
-                daysOld >= 4 -> 5
-                daysOld >= 3 -> 4
-                daysOld >= 2 -> 3
-                daysOld >= 1 -> 2
-                else -> 1
-        }
-
-        //sets the growing plant pitures
-        return when (plant.name.lowercase()) {
-            "elephant" -> when (stage) {
-                1 -> R.drawable.flower_elephant1
-                2 -> R.drawable.flower_elephant2
-                3 -> R.drawable.flower_elephant3
-                4 -> R.drawable.flower_elephant4
-                5 -> R.drawable.flower_elephant5
-                6 -> R.drawable.flower_elephant6
-                else -> R.drawable.flower_elephant1
-            }
-
-            "hibiscus" -> when (stage) {
-                1 -> R.drawable.flower_hibiscus1
-                2 -> R.drawable.flower_hibiscus2
-                3 -> R.drawable.flower_hibiscus3
-                4 -> R.drawable.flower_hibiscus4
-                5 -> R.drawable.flower_hibiscus5
-                6 -> R.drawable.flower_hibiscus5
-                else -> R.drawable.flower_hibiscus1
-            }
-
-            "zebra" -> when (stage) {
-                1 -> R.drawable.flower_zebra1
-                2 -> R.drawable.flower_zebra2
-                3 -> R.drawable.flower_zebra3
-                4 -> R.drawable.flower_zebra4
-                5 -> R.drawable.flower_zebra5
-                6 -> R.drawable.flower_zebra6
-                else -> R.drawable.flower_zebra1
-            }
-
-            "ficus" -> when (stage) {
-                1 -> R.drawable.flower_ficus1
-                2 -> R.drawable.flower_ficus2
-                3 -> R.drawable.flower_ficus3
-                4 -> R.drawable.flower_ficus4
-                5 -> R.drawable.flower_ficus5
-                6 -> R.drawable.flower_ficus6
-                else -> R.drawable.flower_ficus1
-            }
-
-            "coleus" -> when (stage) {
-                1 -> R.drawable.flower_coleus1
-                2 -> R.drawable.flower_coleus2
-                3 -> R.drawable.flower_coleus3
-                4 -> R.drawable.flower_coleus4
-                5 -> R.drawable.flower_coleus5
-                6 -> R.drawable.flower_coleus5
-                else -> R.drawable.flower_coleus1
-            }
-
-            else -> R.drawable.flower_elephant1
-        }
-    }
-
-    //Checks if plant is dead and sets right picture based on stage
-    private fun checkPlantDeath() {
-        pvm.localSessionPlant.value?.let { plant ->
-            if(plant.waterLevel < 30) {
-                val deathStage = getPlantStage(plant)
-
-                binding.imgFlower.setImageResource(getPlantImageId(plant, deathStage))
-                pvm.resetPlantDeathState()
-            }
         }
     }
 }
